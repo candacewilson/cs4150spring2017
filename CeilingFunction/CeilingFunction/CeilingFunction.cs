e70ee030bccc46592a7d1def39c113dd1b35a22e
Candace Wilson <candacemariewilson@gmail.com>
2017-01-27 18:17:01 -0700

    Moved everything into one file

--- CeilingFunction/CeilingFunction/CeilingFunction.cs
+++ CeilingFunction/CeilingFunction/CeilingFunction.cs
@@ -3,7 +3,6 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using static CeilingFunction.BST;
 
 namespace CeilingFunction
 {
@@ -96,5 +95,89 @@ namespace CeilingFunction
                 return (SameTreeShape(x.LeftNode, y.LeftNode) && SameTreeShape(x.RightNode, y.RightNode));
             }
         }
+
+        public class BST
+        {
+            private LeafNode rootNode;
+
+            public BST()
+            {
+                rootNode = null;
+            }
+
+            public LeafNode RootNode
+            {
+                get { return rootNode; }
+                set { rootNode = value; }
+            }
+
+            public void AddLeafNode(int nodeData)
+            {
+                LeafNode newNode = new LeafNode(nodeData);
+                if (rootNode == null)
+                {
+                    rootNode = newNode;
+                    return;
+                }
+
+                LeafNode currentNode = rootNode;
+                LeafNode parentNode = null;
+                while (true)
+                {
+                    parentNode = currentNode;
+                    if (nodeData < currentNode.NodeData)
+                    {
+                        currentNode = currentNode.LeftNode;
+                        if (currentNode == null)
+                        {
+                            parentNode.LeftNode = newNode;
+                            return;
+                        }
+                    }
+                    else
+                    {
+                        currentNode = currentNode.RightNode;
+                        if (currentNode == null)
+                        {
+                            parentNode.RightNode = newNode;
+                            return;
+                        }
+                    }
+                }
+            }
+        }
+
+        public class LeafNode
+        {
+            private int nodeData;
+            private LeafNode leftNode;
+            private LeafNode rightNode;
+
+            public LeafNode(int data)
+            {
+                this.nodeData = data;
+                leftNode = null;
+                rightNode = null;
+            }
+
+            public int NodeData
+            {
+                get { return nodeData; }
+            }
+
+            public LeafNode LeftNode
+            {
+                get { return leftNode; }
+                set { leftNode = value; }
+            }
+
+            public LeafNode RightNode
+            {
+                get { return rightNode; }
+                set { rightNode = value; }
+            }
+        }
+
+
     }
 }
