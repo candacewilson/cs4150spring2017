1c7685ac229107b66fe7a7b1de7ecd41fcd210c6
Candace Wilson <candacemariewilson@gmail.com>
2017-03-30 17:21:58 -0600

    Initial implementation

---
+++ UnderTheRainbow/UnderTheRainbow/UnderTheRainbow.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UnderTheRainbow
+{
+    class UnderTheRainbow
+    {
+        static void Main(string[] args)
+        {
+            String input;
+            int[] distance = null;
+
+            while (true)
+            {
+                if (String.IsNullOrWhiteSpace(input = Console.ReadLine()))
+                {
+                    break;
+                }
+                else
+                {
+                    distance = new int[int.Parse(input) + 1];
+
+                    for(int i = 0; i <= int.Parse(input); i++)
+                    {
+                        distance[i] = int.Parse(Console.ReadLine());
+                    }
+                }
+            }
+
+            Console.WriteLine(MinimumPenalty(distance, 0, new Dictionary<int, int>()));
+            Console.Read();
+        }
+
+        public static int MinimumPenalty(int[] hotels, int start, Dictionary<int, int> cache)
+        {
+            if(cache.ContainsKey(start))
+            {
+                return cache[start];
+            }
+
+            if(start == hotels.Length - 1)
+            {
+                return 0;
+            }
+
+            double penalty = Double.PositiveInfinity;
+            int calculatedPenalty;
+
+            for(int i = hotels.Length - 1; i > start; i--)
+            {
+                calculatedPenalty = (int)Math.Pow(400 - hotels[i] - hotels[start], 2) + MinimumPenalty(hotels, i, cache);
+                penalty = Math.Min(penalty, calculatedPenalty);
+            }
+
+            cache[start] = (int)penalty;
+            return (int)penalty;
+        }
+    }
+}
