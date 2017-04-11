51b340f01e53aa2b0697e0e3a84b8d0bb0b6dcb4
Candace Wilson <candacemariewilson@gmail.com>
2017-04-11 14:12:36 -0600

    Initial implementation

---
+++ SpidermansWorkout/SpidermansWorkout/SpidermansWorkout.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SpidermansWorkout
+{
+    class SpidermansWorkout
+    {
+        static void Main(string[] args)
+        {
+            String input;
+            String[] parsedInput;
+            int[] distances = null;
+            int N = -1;
+            int M;
+
+            while (true)
+            {
+                if (String.IsNullOrWhiteSpace(input = Console.ReadLine()))
+                {
+                    break;
+                }
+                else
+                {
+                    if (N == -1)
+                    {
+                        N = int.Parse(input);
+                    }
+
+                    M = int.Parse(Console.ReadLine());
+
+                    distances = new int[M];
+
+                    for(int i = 0; i < M; i++)
+                    {
+                        distances[i] = int.Parse(Console.ReadLine());
+                    }
+
+                    Console.WriteLine(WerkIt(distances));
+                }
+            }
+
+            Console.Read();
+        }
+
+        public static String WerkIt(int[] distances)
+        {
+            String result = "";
+            int current = 0;
+            int total = 0;
+
+            for(int i = 0; i < distances.Length; i++)
+            {
+                current = distances[i];
+
+                if(total - current >= 0)
+                {
+                    result += "D";
+                    total -= current;
+                }
+                else
+                {
+                    result += "U";
+                    total += current;
+                }
+            }
+
+            if(total != 0)
+            {
+                return "IMPOSSIBLE";
+            }
+
+            return result;
+        }
+    }
+}
