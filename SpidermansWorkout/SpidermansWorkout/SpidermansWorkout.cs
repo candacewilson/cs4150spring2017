af5553d6e4c0fe6b7920d20c142527aa8d721574
Candace Wilson <candacemariewilson@gmail.com>
2017-04-13 11:21:14 -0600

    Used different implementation

--- SpidermansWorkout/SpidermansWorkout/SpidermansWorkout.cs
+++ SpidermansWorkout/SpidermansWorkout/SpidermansWorkout.cs
@@ -11,10 +11,11 @@ namespace SpidermansWorkout
         static void Main(string[] args)
         {
             String input;
+            String[] parsedInput;
             int[] distances = null;
             String[] results = null;
             int N = -1;
-            int count = 0;
+            int M, count = 0;
 
             while (true)
             {
@@ -31,9 +32,16 @@ namespace SpidermansWorkout
                         continue;
                     }
 
-                    distances = Array.ConvertAll(Console.ReadLine().Split(), int.Parse);
+                    M = int.Parse(input);
+                    distances = new int[(M+1)];
+                    parsedInput = Console.ReadLine().Split();
 
-                    results[count] = WerkIt(distances);
+                    for (int i = 1; i <= M; i++)
+                    {
+                        distances[i] = int.Parse(parsedInput[(i-1)]);
+                    }
+
+                    results[count] = WerkIt(distances, M);
                     count++;
                 }
             }
@@ -46,34 +54,76 @@ namespace SpidermansWorkout
             Console.Read();
         }
 
-        public static String WerkIt(int[] distances)
+        public static String WerkIt(int[] distances, int M)
         {
+            int[,] height = new int[41, 1001];
+            int[,] direction = new int[41, 1001];
+            int climbUp, climbDown;
+
+            for(int i = 0; i < 41; i++)
+            {
+                for(int j = 0; j < 1001; j++)
+                {
+                    height[i, j] = -1;
+                }
+            }
+
+            height[0, 0] = 0;
+
+            for(int i = 1; i <= M; i++)
+            {
+                for(int j = 0; j <= (1000 - distances[i]); j++)
+                {
+                    climbUp = j + distances[i];
+                    climbDown = j - distances[i];
+
+                    if(climbDown >= 0)
+                    {
+                        if(height[(i-1), climbUp] != -1 && height[(i-1), climbDown] != -1)
+                        {
+                            height[i, j] = Math.Min(height[(i-1), climbUp], Math.Max(j, height[(i-1), climbDown]));
+                            direction[i, j] = (height[(i-1), climbUp] > Math.Max(j, height[(i-1), climbDown]) ? 1 : -1);
+                        }
+                        else if (height[(i-1), climbUp] != -1)
+                        {
+                            height[i, j] = height[(i-1), climbUp];
+                            direction[i, j] = -1;
+                        }
+                        else if (height[(i-1), climbDown] != -1)
+                        {
+                            height[i, j] = Math.Max(j, height[(i-1), climbDown]);
+                            direction[i, j] = 1;
+                        }
+                    }
+                    else if (height[(i-1), climbUp] != -1)
+                    {
+                        height[i, j] = height[(i-1), climbUp];
+                        direction[i, j] = -1;
+                    }
+                }
+            }
+
+            int[] legalSolution = new int[(M+1)];
+            int position = 0;
+
+            for(int i = M; i >= 1; i--)
+            {
+                legalSolution[i] = direction[i, position];
+                position = position - direction[i, position] * distances[i];
+            }
+
             String result = "";
-            int current = 0;
-            int total = 0;
 
-            for(int i = 0; i < distances.Length; i++)
+            if(height[M, 0] != -1)
             {
-                current = distances[i];
-
-                if(total - current >= 0)
+                for(int i = 1; i <= M; i++)
                 {
-                    result += "D";
-                    total -= current;
+                    result += (legalSolution[i] == 1) ? "U" : "D";
                 }
-                else
-                {
-                    result += "U";
-                    total += current;
-                }
+                return result;
             }
 
-            if(total != 0)
-            {
-                return "IMPOSSIBLE";
-            }
-
-            return result;
+            return "IMPOSSIBLE";
         }
     }
 }
