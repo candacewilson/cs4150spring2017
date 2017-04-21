473d13c7e93b55b0b915943c4c6a1de0a2b495c1
Candace Wilson <candacemariewilson@gmail.com>
2017-04-21 16:07:00 -0600

    Getting input

---
+++ TravelingSalesman/TravelingSalesman/TravelingSalesman.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TravelingSalesman
+{
+    class TravelingSalesman
+    {
+        static void Main(string[] args)
+        {
+            String input;
+            String[] parsedInput;
+            int[,] matrix = null;
+            int N = -1;
+            int count = 0;
+
+            while (true)
+            {
+                if (String.IsNullOrWhiteSpace(input = Console.ReadLine()))
+                {
+                    break;
+                }
+                else
+                {
+                    if (N == -1)
+                    {
+                        N = int.Parse(input);
+                        matrix = new int[N, N];
+                        continue;
+                    }
+
+                    parsedInput = input.Split();
+
+                    for (int i = 0; i < parsedInput.Length; i++)
+                    {
+                        matrix[count, i] = int.Parse(parsedInput[i]);
+                    }
+
+                    count++;
+                }
+            }
+
+            Console.WriteLine("*** running genetic algorithm algorithm ***");
+            Console.Read();
+        }
+    }
+}
