d630660ee5318b8049abb343b8a47ab551a72654
Candace Wilson <candacemariewilson@gmail.com>
2017-05-05 00:56:09 -0600

    movin along

--- GetShorty/GetShorty/GetShorty.cs
+++ GetShorty/GetShorty/GetShorty.cs
@@ -13,10 +13,10 @@ namespace GetShorty
             String input;
             String[] parsedInput;
             List<String> results = new List<String>();
-            Dungeon dungeon = new Dungeon();
-            Dictionary<String, Intersection> intersections = new Dictionary<String, Intersection>();
             int n;
             int m;
+            KeyValuePair<long, double> edge;
+            List<List<KeyValuePair<int, double>>> graph;;
 
             while (true)
             {
@@ -38,37 +38,62 @@ namespace GetShorty
                             break;
                         }
 
-                        for (int i = 0; i < n; i++)
-                        {
-                            Intersection newIntersection = new Intersection(i);
-                            intersections[i.ToString()] = newIntersection;
-                        }
-
+                        graph = new List<List<KeyValuePair<int, double>>>(n);
                         for (int i = 0; i < m; i++)
                         {
-                            input = Console.ReadLine();
-                            parsedInput = input.Split();
-                            intersections[parsedInput[0]].addCorridor(intersections, parsedInput[0], parsedInput[1], float.Parse(parsedInput[2]));
+                            parsedInput = Console.ReadLine().Split();
+                            int x = int.Parse(parsedInput[0]);
+                            int y = int.Parse(parsedInput[1]);
+                            double factor = double.Parse(parsedInput[2]);
+
+                            graph[x].Add(new KeyValuePair<int, double> ( y, factor ));
+                            graph[y].Add(new KeyValuePair<int, double> ( x, factor ));
+
                         }
 
-                        foreach (String key in intersections.Keys)
+                        List<double> maxSizes = Enumerable.Repeat(double.MinValue, n).ToList();
+                        maxSizes[0] = 1;
+
+                        IntersectionCorridorComparer comparison = new IntersectionCorridorComparer();
+
+                        SortedSet<KeyValuePair<int, double>> q = new SortedSet<KeyValuePair<int, double>>(comparison);
+
+                        q.Add(new KeyValuePair<int, double>(0, 1));
+
+                        while(q.Count != 0)
                         {
-                            dungeon.addIntersection(key, intersections[key].GetAllCorridors());
+                            var u = q.First().Key;
+                            q.Remove(q.First());
+
+                            for(var p in graph[u])
+                            {
+
+                            }
                         }
 
-                        List<String> Path = dungeon.Dijkstras("0", (n - 1).ToString());
-                        String startingIntersection = "0";
-                        float totalFactor = 1.0000f;
 
-                        foreach (String corridor in Path)
+
+                        while (!q.empty())
                         {
-                            Dictionary<String, float> corridors = intersections[startingIntersection].GetAllCorridors();
-                            totalFactor = totalFactor * corridors[corridor];
-                            startingIntersection = corridor;
+                            auto u = q.begin()->first;
+                            q.erase(q.begin());
+
+                            for (auto p : g[u])
+                            {
+                                auto v = p.first;
+                                auto fraction = max_sizes[u] * p.second;
+                                if (fraction > max_sizes[v])
+                                {
+                                    q.erase(make_pair(v, max_sizes[v]));
+                                    q.insert(make_pair(v, fraction));
+                                    max_sizes[v] = fraction;
+                                }
+                            }
                         }
 
-                        Math.Round((Decimal)totalFactor, 4, MidpointRounding.AwayFromZero);
-                        results.Add(Math.Round((Decimal)totalFactor, 4, MidpointRounding.AwayFromZero).ToString("n4"));
+                        results.Add(Math.Round((Decimal)maxSizes[n-1], 4, MidpointRounding.AwayFromZero).ToString("n4"));
+
+                        cout << fixed << setprecision(4) << max_sizes[n - 1] << endl;
                     }
                 }
             }
@@ -81,111 +106,209 @@ namespace GetShorty
             Console.Read();
         }
 
-        public class Intersection
+
+
+        
+
+        public class IntersectionCorridorComparer : IComparer<KeyValuePair<long, double>>
         {
-            private int intersection;
-            private Dictionary<String, float> corridors = new Dictionary<String, float>();
-
-            public Intersection(int intersection)
+            public int Compare(KeyValuePair<long, double> x, KeyValuePair<long, double> y)
             {
-                this.intersection = intersection;
-            }
-
-            public void addCorridor(Dictionary<String, Intersection> intersections, String source, String target, float factor)
-            {
-                if (corridors.ContainsKey(target) || intersections[target].corridors.ContainsKey(source))
-                {
-                    if (corridors[target] < factor)
-                    {
-                        corridors[target] = factor;
-                        intersections[target].corridors[source] = factor;
-                    }
-
-                    if (intersections[target].corridors[source] < factor)
-                    {
-                        intersections[target].corridors[source] = factor;
-                    }
-                }
-
-                corridors[target] = factor;
-                intersections[target].corridors[source] = factor;
-            }
-
-            public Dictionary<String, float> GetAllCorridors()
-            {
-                return corridors;
+                return Convert.ToInt32(x.Value > y.Value || (x.Value == y.Value && x.Key > y.Key));
             }
         }
 
-        public class Dungeon
-        {
-            Dictionary<String, Dictionary<String, float>> intersectionsAndCorridors = new Dictionary<String, Dictionary<String, float>>();
 
-            public void addIntersection(String intersection, Dictionary<String, float> corridors)
-            {
-                intersectionsAndCorridors[intersection] = corridors;
-            }
 
-            public List<String> Dijkstras(String sourceIntersection, String targetIntersection)
-            {
-                var prev = new Dictionary<String, String>();
-                var factors = new Dictionary<String, float>();
-                var intersections = new List<String>();
 
-                List<String> path = null;
 
-                foreach (var intersection in intersectionsAndCorridors)
-                {
-                    if (intersection.Key == sourceIntersection)
-                    {
-                        factors[intersection.Key] = 1;
-                    }
-                    else
-                    {
-                        factors[intersection.Key] = float.MinValue;
-                    }
 
-                    intersections.Add(intersection.Key);
-                }
 
-                while (intersections.Count != 0)
-                {
-                    intersections.OrderBy(a => a).ToArray();
 
-                    var nextIntersection = intersections[0];
-                    intersections.Remove(nextIntersection);
 
-                    if (nextIntersection == targetIntersection)
-                    {
-                        path = new List<String>();
-                        while (prev.ContainsKey(nextIntersection))
-                        {
-                            path.Add(nextIntersection);
-                            nextIntersection = prev[nextIntersection];
-                        }
 
-                        break;
-                    }
 
-                    if (factors[nextIntersection] == float.MinValue)
-                    {
-                        break;
-                    }
 
-                    foreach (var neighbor in intersectionsAndCorridors[nextIntersection])
-                    {
-                        var alt = factors[nextIntersection] * neighbor.Value;
-                        if (alt > factors[neighbor.Key])
-                        {
-                            factors[neighbor.Key] = alt;
-                            prev[neighbor.Key] = nextIntersection;
-                        }
-                    }
-                }
 
-                path.Reverse();
-                return path;
-            }
-        }
+
+
+        //static void Main(string[] args)
+        //{
+        //    String input;
+        //    String[] parsedInput;
+        //    List<String> results = new List<String>();
+        //    Dungeon dungeon = new Dungeon();
+        //    Dictionary<int, Intersection> intersections = new Dictionary<int, Intersection>();
+        //    int n;
+        //    int m;
+
+        //    while (true)
+        //    {
+        //        if (String.IsNullOrWhiteSpace(input = Console.ReadLine()))
+        //        {
+        //            break;
+        //        }
+        //        else
+        //        {
+        //            parsedInput = input.Split();
+
+        //            if (parsedInput.Count() == 2)
+        //            {
+        //                n = int.Parse(parsedInput[0]);
+        //                m = int.Parse(parsedInput[1]);
+
+        //                if (n == 0 && m == 0)
+        //                {
+        //                    break;
+        //                }
+
+        //                for (int i = 0; i < n; i++)
+        //                {
+        //                    Intersection newIntersection = new Intersection(i);
+        //                    intersections[i] = newIntersection;
+        //                }
+
+        //                for (int i = 0; i < m; i++)
+        //                {
+        //                    input = Console.ReadLine();
+        //                    parsedInput = input.Split();
+        //                    intersections[int.Parse(parsedInput[0])].addCorridor(intersections, int.Parse(parsedInput[0]), int.Parse(parsedInput[1]), float.Parse(parsedInput[2]));
+        //                }
+
+        //                foreach (int key in intersections.Keys)
+        //                {
+        //                    dungeon.addIntersection(key, intersections[key].GetAllCorridors());
+        //                }
+
+        //                List<int> Path = dungeon.Dijkstras(0, n-1);
+        //                int startingIntersection = 0;
+        //                float totalFactor = 1.0000f;
+
+        //                foreach (int corridor in Path)
+        //                {
+        //                    Dictionary<int, float> corridors = intersections[startingIntersection].GetAllCorridors();
+        //                    totalFactor = totalFactor * corridors[corridor];
+        //                    startingIntersection = corridor;
+        //                }
+
+        //                Math.Round((Decimal)totalFactor, 4, MidpointRounding.AwayFromZero);
+        //                results.Add(Math.Round((Decimal)totalFactor, 4, MidpointRounding.AwayFromZero).ToString("n4"));
+        //            }
+        //        }
+        //    }
+
+        //    foreach (String value in results)
+        //    {
+        //        Console.WriteLine(value);
+        //    }
+
+        //    Console.Read();
+        //}
+
+        //public class Intersection
+        //{
+        //    private int intersection;
+        //    private Dictionary<int, float> corridors = new Dictionary<int, float>();
+
+        //    public Intersection(int intersection)
+        //    {
+        //        this.intersection = intersection;
+        //    }
+
+        //    public void addCorridor(Dictionary<int, Intersection> intersections, int source, int target, float factor)
+        //    {
+        //        if (corridors.ContainsKey(target) || intersections[target].corridors.ContainsKey(source))
+        //        {
+        //            if (corridors[target] < factor)
+        //            {
+        //                corridors[target] = factor;
+        //                intersections[target].corridors[source] = factor;
+        //            }
+
+        //            if (intersections[target].corridors[source] < factor)
+        //            {
+        //                intersections[target].corridors[source] = factor;
+        //            }
+        //        }
+
+        //        corridors[target] = factor;
+        //        intersections[target].corridors[source] = factor;
+        //    }
+
+        //    public Dictionary<int, float> GetAllCorridors()
+        //    {
+        //        return corridors;
+        //    }
+        //}
+
+        //public class Dungeon
+        //{
+        //    Dictionary<int, Dictionary<int, float>> intersectionsAndCorridors = new Dictionary<int, Dictionary<int, float>>();
+
+        //    public void addIntersection(int intersection, Dictionary<int, float> corridors)
+        //    {
+        //        intersectionsAndCorridors[intersection] = corridors;
+        //    }
+
+        //    public List<int> Dijkstras(int sourceIntersection, int targetIntersection)
+        //    {
+        //        Dictionary<int, int> prev = new Dictionary<int, int>();
+        //        Dictionary<int, float> factors = new Dictionary<int, float>();
+        //        List<int> intersections = new List<int>();
+
+        //        List<int> path = new List<int>(); 
+
+        //        foreach (var intersection in intersectionsAndCorridors)
+        //        {
+        //            if (intersection.Key == sourceIntersection)
+        //            {
+        //                factors[intersection.Key] = 1;
+        //            }
+        //            else
+        //            {
+        //                factors[intersection.Key] = float.MinValue;
+        //            }
+
+        //            intersections.Add(intersection.Key);
+        //        }
+
+        //        while (intersections.Count != 0)
+        //        {
+        //            intersections.OrderBy(a => a).ToArray();
+
+        //            int currentIntersection = intersections[0];
+        //            intersections.Remove(currentIntersection);
+
+        //            if (currentIntersection == targetIntersection)
+        //            {
+        //                while (prev.ContainsKey(currentIntersection))
+        //                {
+        //                    path.Add(currentIntersection);
+        //                    currentIntersection = prev[currentIntersection];
+        //                }
+
+        //                break;
+        //            }
+
+        //            if (factors[currentIntersection] == float.MinValue)
+        //            {
+        //                break;
+        //            }
+
+        //            foreach (var neighbor in intersectionsAndCorridors[currentIntersection])
+        //            {
+        //                var alt = factors[currentIntersection] * neighbor.Value;
+        //                if (alt > factors[neighbor.Key])
+        //                {
+        //                    factors[neighbor.Key] = alt;
+        //                    prev[neighbor.Key] = currentIntersection;
+        //                }
+        //            }
+        //        }
+
+        //        path.Reverse();
+        //        return path;
+        //    }
+        //}
     }
 }
